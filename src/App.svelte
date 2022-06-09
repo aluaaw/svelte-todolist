251418b798f38f1185f42bf8e97b80746d37220c
aluaaw <ylua.aw@gmail.com>
2022-06-09 16:34:37 +0900

    init, practice todolist v1

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let name;
+  import Todolist from "./Todolist.svelte";
+  import { todolist } from "./store";
+</script>
+
+<main>
+  <h3>List : {name}</h3>
+  <h4>Add Your Todo</h4>
+  <Todolist />
+
+  <h4>Your List</h4>
+  {#each $todolist as todo, i}
+    <div>
+      <input type="checkbox" />
+      {i + 1}. {todo}
+    </div>
+  {/each}
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h3 {
+    color: green;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Todolist.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { todolist } from "./store";
+  let todo = "";
+  let list = [];
+
+  function handleKeypress({ key }) {
+    key === "Enter" && addTodo();
+  }
+
+  function addTodo() {
+    list.push(todo);
+    $todolist = list;
+    todo = "";
+  }
+</script>
+
+<main>
+  <input
+    type="text"
+    placeholder="write here"
+    on:change={(e) => (todo = e.target.value)}
+    bind:value={todo}
+    on:keypress={(e) => handleKeypress(e)}
+  />
+  <button on:click={addTodo}>ADD</button>
+</main>
